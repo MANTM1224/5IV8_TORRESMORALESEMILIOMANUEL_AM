1f9b547567f4101d3dc7892e13d4b6394edb32d6
MANTM1224 <etorresm2304@alumno.ipn.mx>
2025-09-29 15:22:46 -0600

    Se agrega la app blanca

---
+++ aplicacion vacia/aplicacionvacia/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hola JEJE</Text>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'bl',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
